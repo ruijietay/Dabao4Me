18106e2e1fe99448a0e663fe6b9b1369214bcb36
dillon <e0958762@u.nus.edu>
2023-05-20 17:34:50 +0800

    testing branch

--- testBot.py
+++ testBot.py
@@ -5,8 +5,11 @@ import telegram
 async def main():
     bot = telegram.Bot("5937019735:AAGHDxML3WPmbSuIw4NxPxeww0UP5Wezvw0")
     async with bot:
-        print(await bot.get_me())
+        print((await bot.get_updates())[0].message.from_user.id)
+        await bot.send_message(text="Hi", chat_id=(await bot.get_updates())[0].message.from_user.id)
 
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+#update to test branch
